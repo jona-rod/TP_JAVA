d5d087bceccd24df0c4530b9b92baebee59be36f
maximo-cobeaga <maximocobeaga@gmail.com>
2025-06-09 18:24:34 -0300

    Defino atributos, creo setters y getters en clases: zonas, restringida, stand

--- src/LOGICA/Comun.java
+++ src/LOGICA/Comun.java
@@ -1,8 +1,13 @@
 package LOGICA;
 
 public class Comun extends Zona {
+    int capacidadMax;
 
     public Comun(String codigo, String descripcion) {
         super(codigo, descripcion);
     }
+
+    public void setCapacidadMax(int capacidadMax) {this.capacidadMax = capacidadMax;}
+
+    public int getCapacidadMax() {return capacidadMax;}
 }
