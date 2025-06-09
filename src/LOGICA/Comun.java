2956ef8b652380ff25bd5044323d74cd1372153d
Franasensio21 <franciscoasensiobethouart@gmail.com>
2025-06-09 20:09:10 -0300

    getters y setters de evento, escenario y comun y metodos to string

--- src/LOGICA/Comun.java
+++ src/LOGICA/Comun.java
@@ -5,4 +5,9 @@ public class Comun extends Zona {
     public Comun(String codigo, String descripcion) {
         super(codigo, descripcion);
     }
+    int CapacidadMax;
+    @Override
+    public String toString() {
+        return "Zona Común [" + codigo + "] " + descripcion;
+    }
 }
