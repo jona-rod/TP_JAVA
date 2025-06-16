c8c81b0c7c99e454abac5666c9c372efc464860b
jona-rod <rodriguez.jonathan.ing@gmail.com>
2025-06-16 12:54:31 -0300

    .

--- src/LOGICA/LISTADOS/ListadoStands.java
+++ src/LOGICA/LISTADOS/ListadoStands.java
@@ -3,6 +3,9 @@ package LOGICA.LISTADOS;
 import LOGICA.GESTION.Gestion;
 import LOGICA.ZONAS.Stand;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Collections;
 
 
@@ -16,6 +19,15 @@ public class ListadoStands {
         for (Stand s : gestion.getListadoStands()) {
             sb.append(s.toString()).append("\n");
         }
+        try{
+            FileWriter file = new FileWriter("src//LOGICA/ARCHIVOS/listadoStands.txt");
+            BufferedWriter writer = new BufferedWriter(file);
+            writer.write(sb.toString());
+            writer.close();
+        }catch(IOException e){
+            System.err.println("Error al escribir el archivo: " + e.getMessage());
+        }
+
         return sb.toString();
     }
 }
--- src/LOGICA/LISTADOS/ListadoZonas.java
+++ src/LOGICA/LISTADOS/ListadoZonas.java
@@ -3,11 +3,14 @@ package LOGICA.LISTADOS;
 import LOGICA.GESTION.Gestion;
 import LOGICA.ZONAS.Zona;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Collections;
 
 public class ListadoZonas {
 
-    public String generarListado(Gestion gestion) {
+    public String generaListado(Gestion gestion) {
         int cantPersonas = 0;
 
         Collections.sort(gestion.getListadoZonas()); // Usa compareTo de Zona
@@ -17,8 +20,17 @@ public class ListadoZonas {
             sb.append(z.toString()).append("\n");
             cantPersonas += z.concurrencia();
         }
-
         sb.append("La cantidad de personas en el predio es: ").append(cantPersonas);
+
+        try{
+            FileWriter file = new FileWriter("src//LOGICA/ARCHIVOS/listadoZonas.txt");
+            BufferedWriter writer = new BufferedWriter(file);
+            writer.write(sb.toString());
+            writer.close();
+        }catch(IOException e){
+            System.err.println("Error al escribir el archivo: " + e.getMessage());
+        }
+
         return sb.toString();
     }
 
