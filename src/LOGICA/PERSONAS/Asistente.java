6934a58ce0f7138f4f70def965d103d5e7ea563f
Franasensio21 <franciscoasensiobethouart@gmail.com>
2025-06-18 18:19:43 -0300

    .

--- src/LOGICA/PERSONAS/Asistente.java
+++ src/LOGICA/PERSONAS/Asistente.java
@@ -16,7 +16,7 @@ public class Asistente extends Persona {
 
     @Override
     public boolean habilitado(Zona zona) {
-        return zonaHabilitada(zona) || zona.tipoZona() == 'C' ||zona.tipoZona() == 'E' || zona.tipoZona() == 'S';
+        return zonaHabilitada(zona) || zona.tipoZona() == 'C' ||zona.tipoZona() == 'E';
     }
 
 
