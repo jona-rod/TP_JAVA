27bafa8de606eb4b077e57699c961eb67442488e
jona-rod <rodriguez.jonathan.ing@gmail.com>
2025-06-19 18:50:21 -0300

    agrego VentanaMuevePersonas

--- src/IGU/VentanaMuevePersona.java
+++ src/IGU/VentanaMuevePersona.java
@@ -3,19 +3,39 @@ package IGU;
 import LOGICA.GESTION.Gestion;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class VentanaMuevePersona extends JFrame{
     private JPanel ventana_mueve_personas;
-    private JTextPane textPane1;
-    private JTextPane area_texto_zonas;
-    private JTextArea textArea1;
     private JTextArea area_texto_personas;
     private JTextField campoi_id_persona;
     private JTextField campor_id_zona;
     private JButton btn_mover;
+    private JTextArea area_texto_zonas;
 
     public VentanaMuevePersona(Gestion gestion) {
         add(ventana_mueve_personas);
         this.setSize(1300, 800);
+
+        area_texto_zonas.setText(gestion.getConjuntoZonas().toString());
+        area_texto_personas.setText(gestion.muestraListadoPersonas().toString());
+
+
+
+        btn_mover.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    gestion.muevePersona(campoi_id_persona.getText(), campor_id_zona.getText());
+                    JOptionPane.showMessageDialog(null, "Se realiza cambio de zona con exito");
+                    area_texto_zonas.setText(gestion.getConjuntoZonas().toString());
+                    area_texto_personas.setText(gestion.muestraListadoPersonas().toString());
+
+                    }catch (Exception ex){
+                    JOptionPane.showMessageDialog(null, ex.getMessage());
+                    }
+                }
+        });
     }
 }
